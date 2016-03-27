a211d18154e014623c65f6793a1710f2b6124026
Emerson Rocha Luiz <emerson@alligo.com.br>
2016-03-27 00:35:27 -0300

    Renamed from start code; clean up

--- mod_varnish_purge.php
+++ mod_varnish_purge.php
@@ -1,21 +1,15 @@
 <?php
 /**
  * @package     Alligo.Modules
- * @subpackage  mod_banners4varnish
+ * @subpackage  mod_varnish_purge
  *
  * @copyright   Copyright (C) 2005 - 2015 Alligo Ltda. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
 
-/**
- * Helper for mod_banners4varnish
- *
- * @package     Alligo.Modules
- * @subpackage  mod_banners4varnish
- * @since       3.4
- */
-class ModVarnishPurgeHelper
-{
+// Include the syndicate functions only once
+require_once __DIR__ . '/helper.php';
 
-}
+require JModuleHelper::getLayoutPath('mod_varnish_purge', $params->get('layout', 'default'));
